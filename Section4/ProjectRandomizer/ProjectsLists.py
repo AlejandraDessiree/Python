d197772ea32aa06b952538d3cfc1cfc7b54e9db7
AlejandraDessiree <alejandradessiree@gmail.com>
2024-11-19 19:38:50 -0500

    second commit

--- Section4/ProjectRandomizer/ProjectsLists.py
+++ Section4/ProjectRandomizer/ProjectsLists.py
@@ -1,7 +1,6 @@
+import random
+
 states_of_America = ["Pennsilvania", "Florida", "Delaware", "Arkansas"]
-
-print(states_of_America)
-
 states_of_America.extend(["Miami", "Orlando", "Iowa", "Greenland"])
 
-print(states_of_America[5])
+print(random.choice(states_of_America))
