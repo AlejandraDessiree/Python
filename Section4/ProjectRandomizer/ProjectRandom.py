495852432cbf95b3d9e6f46b2f15c3b6e4b3cfe1
AlejandraDessiree <alejandradessiree@gmail.com>
2024-11-20 12:33:22 -0500

    third commit

--- Section4/ProjectRandomizer/ProjectRandom.py
+++ Section4/ProjectRandomizer/ProjectRandom.py
@@ -1,11 +1,11 @@
 from ProjectModule import player_2
 
 print("Lets play Rock, Paper and Siccors")
-player_1 = input("Rock is \"1\", Paper is \"2\" and Siccors is \"3\"")
+player_1 = input("Rock is \"1\", Paper is \"2\" and Siccors is \"3\"\n")
 
 while player_1 not in ["1", "2", "3"]:
     print("Please use \"1\" for Rock, \"2\" for Paper and \"3\" for Siccors")
-    player_1 = input("Rock is \"1\", Paper is \"2\" and Siccors is \"3\"")
+    player_1 = input("Rock is \"1\", Paper is \"2\" and Siccors is \"3\"\n")
     
 player_1 = int(player_1)
 player_2 = player_2()
@@ -114,4 +114,4 @@ elif player_1 == 3:
 | SCISSORS |
  \         /
   \_______/''')
-        print("TIe")
+        print("Tie")
---
+++ Section5/Loops/PasswordsGen.py
@@ -0,0 +1,27 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to My Password Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_numbers = int(input(f"How many numbers you want in your password?\n"))
+nr_symbols = int(input(f"How manu symbols you want in your password?\n"))
+
+password = []
+
+for char in range(1, nr_letters + 1):
+    random_character = random.choice(letters)
+    password += random_character
+    
+for num in range(1, nr_numbers + 1):
+    random_number = random.choice(numbers)
+    password += random_number
+     
+for sym in range(1, nr_symbols + 1):
+    random_symbol = random.choice(symbols)
+    password +=  random_symbol
+    
+random.shuffle(password)
+print("".join(password))
