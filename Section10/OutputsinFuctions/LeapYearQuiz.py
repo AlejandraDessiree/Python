dc3e1defb63e945d2e1d29b6f9eb7133720deea4
AlejandraDessiree <alejandradessiree@gmail.com>
2024-11-24 00:09:00 -0500

    Finished

--- Section10/OutputsinFuctions/LeapYearQuiz.py
+++ Section10/OutputsinFuctions/LeapYearQuiz.py
@@ -29,13 +29,3 @@ print(result)
 # This is
 # a comment
 # to try ctr + /
-
-def my_function(a):
-    if a < 40:
-        return
-        print("Terrible")
-    if a < 80:
-        return "Pass"
-    else:
-        return "Great"
-print(my_function(25))
