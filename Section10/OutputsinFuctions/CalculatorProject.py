ecaa42023ae5dc0e33846fb0a0d2fa27d0e09007
AlejandraDessiree <alejandradessiree@gmail.com>
2024-11-24 00:27:58 -0500

    Finished

--- Section10/OutputsinFuctions/CalculatorProject.py
+++ Section10/OutputsinFuctions/CalculatorProject.py
@@ -16,7 +16,6 @@ Calc = """
 |_____________________|"""
 
 print(Calc)
-
 def calculator(result, num_2):
     pick = str(input("Pick an operation:\nSum = '+'\nMinus = '-'\nDivide = '/'\nMultiplication = '*'\n"))
     num_1 = result
@@ -30,7 +29,10 @@ def calculator(result, num_2):
     elif pick == "-":
         result = num_1 - num_2
     elif pick == "/":
-        result = num_1 / num_2
+        if num_2 == 0:
+            return "You cannot divide by zero, I cannot program that fucnyion :("
+        else:
+            result = num_1 / num_2
     elif pick == "*":
         result = num_1 * num_2
         
@@ -43,35 +45,11 @@ def calculator(result, num_2):
        
     while second_pick in ["y", "n", "stop"]:
         if second_pick == "y":
-            num_1 = result
-            num_2 = float(input("What's your second number?\n"))
-            pick = str(input("Pick an operation:\nSum = '+'\nMinus = '-'\nDivide = '/'\nMultiplication = '*'\n")).lower()
-            
-            
-            while pick not in ["+", "-", "/", "*"]:
-                print("I do not recognize that pick")
-                pick = str(input("Pick an operation:\nSum = '+'\nMinus = '-'\nDivide = '/'\nMultiplication = '*'\n")).lower()
-                
-            if pick == "+":
-                result = num_1 + num_2
-            elif pick == "-":
-                result = num_1 - num_2
-            elif pick == "/":
-                result = num_1 / num_2
-            elif pick == "*":
-                result = num_1 * num_2
-            
-                
-            print(f"{num_1} {pick} {num_2} = {result}")
-            second_pick = input(f"Type 'y' to continue calculating with {result} or type 'n' to start a new calculation, or type 'stop' to stop the program:\n").lower()
-            return result
+            return calculator(result, float(input("What's your second number?\n")))
         elif second_pick == "n":
            return calculator(float(input("What's your first number?\n")), float(input("What's your second number?\n")))
         elif second_pick == "stop":
             return " "
     
-    
 output = calculator(float(input("What's your first number?\n")), float(input("What's your second number?\n")))
-
-
 print(output)
